ff5eed133189cfe265d75a44f0e45697ba7b5c09
Thiện <125804715+shuneo004@users.noreply.github.com>
2024-04-18 18:41:48 +0700

    update test

--- test.cpp
+++ test.cpp
@@ -1,19 +1,7 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// Custom comparison struct for pairs
-    // Define value_type typedef required by std::vector
-
-    bool cmp (pair<int,int>lhs, pair<int,int> rhs)  {
-        if (lhs.second - lhs.first == rhs.second - rhs.first) return lhs.first < rhs.first;
-        return lhs.second - lhs.first > rhs.second - rhs.first;
-    }
-
-
 int main() {
-    vector<pair<int,int>> a;
-    sort(a.begin(), a.end(), cmp);
-    // Rest of your code...
+
     return 0;
 }
